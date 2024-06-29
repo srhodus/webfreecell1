625c709732ae06015b436948893310140459aa65
srhodus <s.rhodus@yahoo.com>
2024-06-29 09:30:40 -0500

    Convert to nodejs

--- js/main.js
+++ js/main.js
@@ -12,35 +12,20 @@ function createTable() {
     };
 }
 
+function createTableFromJson(desc) {
+    return JSON.parse(desc);
+}
+
 if (typeof window === 'undefined') {
 (function() {
     try {
-        var stdin = new java.io.BufferedReader(new java.io.InputStreamReader(
-            java.lang.System.in));
-        var buf;
-        var i = 0;
-        var table = createTable();
-        var solution = [];
-        let cascades = true;
-        while (buf = stdin.readLine()) {
-            var ar = buf.trim().split(" ");
-            if (ar[0] == '.') {
-                cascades = false;
-                i = 0;
-                continue;
-            }
-            if (cascades) {
-                table.cascades[i] = ar;
-                i++;
-            } else {
-                solution.push(ar);
-            }
-        }
-        print(JSON.stringify(table));
-        print(JSON.stringify(solution));
+        const fs = require("fs");
+        var buf = fs.readFileSync(0);
+        var table = createTableFromJson(buf);
+        console.log(JSON.stringify(table));
     } catch (e) {
-        print("Caught exception:");
-        print(e.stack);
+        console.log("Caught exception:");
+        console.log(e.stack);
     }
 }());
 }
