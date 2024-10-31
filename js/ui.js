1ff44103f7c6a27105730b9bfe900d33c2f64b72
srhodus <s.rhodus@yahoo.com>
2024-10-31 09:55:59 -0500

    WIP

--- js/ui.js
+++ js/ui.js
@@ -12,12 +12,17 @@ var ready = (callback) => {
 ready(() => {
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
+	var image = document.getElementById("texture");
 	ctx.canvas.width = window.innerWidth;
 	ctx.canvas.height = window.innerHeight;
+	var table = createTable();
+	console.log(JSON.stringify(table));
+
 	function draw() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		ctx.fillStyle = "DarkGreen";
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		ctx.drawImage(image, 0, 0, 936, 384);
 		window.requestAnimationFrame(draw);
 	}
 	window.requestAnimationFrame(draw);
